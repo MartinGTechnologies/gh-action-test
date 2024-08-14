b2aadb20c35453187c8af2bf42557532bd041eda
mgranger <martin.granger@siriusxm.com>
2024-08-14 11:37:48 -0400

    adjust file locations

---
+++ rust-tenantizer/src/main.rs
@@ -0,0 +1,60 @@
+use std::env;
+use std::fs;
+use std::path::{Path};
+use tera::{Tera, Context};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: <facility_name> <base_url>");
+        return;
+    }
+
+    let facility_name = &args[1];
+    let base_url = format!("https://{}", &args[2]);
+
+    let parent_folder = Path::new("tenants");
+    let folders = vec![
+        parent_folder.join(facility_name),
+        parent_folder.join(format!("{}-TEST", facility_name)),
+    ];
+
+    let file_templates = vec![
+        ("environment.tenant.ts", "environment.tenant.ts"),
+        ("apple.appid.json", "apple.appid.json"),
+        ("datadog.json", "datadog.json"),
+    ];
+
+
+    //create an instance of Tera
+    let tera = Tera::new("templates/**/*").expect("Failed to initialize Tera");
+
+    for folder in folders {
+        if folder.exists() {
+            println!("Folder {} already exists. Skipping creation.", folder.display());
+            continue;
+        }
+
+        fs::create_dir_all(&folder).expect("Failed to create directory");
+
+        let site_name = folder.file_name().unwrap().to_str().unwrap();
+
+        for (template_name, file_name) in &file_templates {
+            let file_path = folder.join(file_name);
+
+            let app_id_suffix = if site_name.ends_with("-TEST") {
+                format!("{}.test", facility_name.to_lowercase())
+            } else {
+                facility_name.to_lowercase()
+            };
+
+            let mut context = Context::new();
+            context.insert("base_url", &base_url);
+            context.insert("site_name", site_name);
+            context.insert("app_id_suffix", &app_id_suffix);
+
+            let content = tera.render(template_name, &context).expect("Failed to render template");
+            fs::write(&file_path, content).expect("Failed to write file");
+        }
+    }
+}
---
+++ templates/environment.tenant.ts
@@ -0,0 +1,5 @@
+import { defineEnvironment } from "src/environments/common";
+
+export const environment = defineEnvironment({
+    baseUrl: "{{ base_url }}",
+});
